6abaee19e27997b03c1de19dd1c86220dfdff441
mcairco2 <166839881+mcairco2@users.noreply.github.com>
2025-09-05 13:23:39 -0400

    Add files via upload

--- product_catalog.py
+++ product_catalog.py
@@ -1,27 +1,35 @@
 from product_data import products
 # TODO: Step 1 - Print out the products to see the data that you are working with.
-
+print("Product Catalog Preview:")
+for p in products[:3]: #printing jus tthe first 3 products to preview
+    print(p)
 
 
 # TODO: Step 2 - Create a list called customer_preferences and store the user preference in this list.
-
+customer_preferences = []
 
 response = ""
 while response != "N":
     print("Input a preference:")
-    preference = input()
+    preference = input().strip().lower()
     # Add the customer preference to the list
+    if preference:
+        customer_preferences.append(preference)
 
     response = input("Do you want to add another preference? (Y/N): ").upper()
   
 
 # TODO: Step 3 - Convert customer_preferences list to set to eliminate duplicates.
-
+customer_preferences = set(customer_preferences)
 
 
 # TODO: Step 4 - Convert the product tags to sets in order to allow for faster comparisons.
 converted_products = []
-
+for product in products:
+    converted_products.append({
+        "name": product["name"],
+        "tags": set(product["tags"])
+    })
 
 
 
@@ -34,7 +42,7 @@ def count_matches(product_tags, customer_tags):
     Returns:
         int: The number of matching tags between the product and customer.
     '''
-    pass
+    return len(product_tags.intersection(customer_tags))
 
 
 
@@ -48,11 +56,23 @@ def recommend_products(products, customer_tags):
     Returns:
         list: A list of products containing product names and their match counts.
     '''
-    pass
+    recommendations = []
+    for product in products:
+        matches = count_matches(product["tags"], customer_tags)
+        if matches > 0:
+            recommendations.append((product["name"], matches))
+    #sort recommendations by match count
+    recommendations.sort(key=lambda x: x[1], reverse=True)
+    return recommendations
 
 
 
 # TODO: Step 7 - Call your function and print the results
+results = recommend_products(converted_products, customer_preferences)
+
+print("\nRecommend Products:")
+for name, matches in results:
+    print(f"- {name} ({matches} match(es))")
 
 
 
